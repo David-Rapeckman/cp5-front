e3c550053b6c220d9c04e0e1a15ffe8c6f4e1802
Gugomesx10 <gustavogomesmartins4@icloud.com>
2024-10-01 22:33:58 -0300

    Ajeitando o 404

--- cp5/src/pages/404.tsx
+++ cp5/src/pages/404.tsx
@@ -6,7 +6,7 @@ export default function Custom404() {
       <h1 className="text-6xl font-bold">404 - Page Not Found</h1>
       <Image
         src="/error-image.jpg"
-        alt="Error Image"
+        alt="Error "
         width={500}
         height={500}
         className="mt-6"
