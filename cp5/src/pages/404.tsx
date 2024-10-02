20b653c3a45081ab21900fd6ba2773582dadcc4a
Gugomesx10 <gustavogomesmartins4@icloud.com>
2024-10-01 22:35:33 -0300

    Ajeitando 404 novamente!

--- cp5/src/pages/404.tsx
+++ cp5/src/pages/404.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Custom404() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-6xl font-bold">404 - Page Not Found</h1>
+      <h1 className="">404 - Page Not Found</h1>
       <Image
         src="/error-image.jpg"
         alt="Error image"
